c5426a77bd0f84a884f446ee24922f47090444fb
Geraldo <profgeraldofip@gmail.com>
2022-02-10 07:33:50 -0300

    Gerado entidade user

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -3,6 +3,6 @@ import { PrismaService } from './prisma.service';
 
 @Module({
   providers: [PrismaService],
-  export: [PrismaService],
+  exports: [PrismaService],
 })
 export class PrismaModule {}
